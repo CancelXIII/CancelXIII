04013823dca4721adf7fdbc5d75f729ca2c3b318
Cancel XIII <167734376+CancelXIII@users.noreply.github.com>
2024-05-01 17:08:08 +0300

    Update telgram_bot_private_puplic_start_command.py

--- telgram_bot_private_puplic_start_command.py
+++ telgram_bot_private_puplic_start_command.py
@@ -1,21 +1,23 @@
-start_private_msg = ' welcome to private chat'
-start_supergroup_msg = ' welcome to our public chat'
 
-help_msg = 'how help you?'
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
+    start_private_msg = ' welcome to private chat'
+    help_private_msg = 'how help you?'
+    start_supergroup_msg = f' welcome to our {message.chat.title} chat'
+    help_supergroup_msg = 'how help you?'
     if message.chat.type == 'supergroup':
-        start_command = '/start'+'@nameOFBot'
-        help_command = '/help'+'@nameOFBot'
+        start_command = '/start'+'@MrMemosBot'
+        help_command = '/help'+'@MrMemosBot'
         if message.text == start_command:
-            bot.reply_to(message, f' welcome to our {message.chat.title} chat')
+            bot.reply_to(message, start_supergroup_msg)
         elif message.text == help_command:
-            bot.reply_to(message, help_msg)
+            bot.reply_to(message, help_supergroup_msg)
+        #set option to /mention_admin to send (help message) to online admins
     elif message.chat.type == 'private':
         start_command = '/start'
         help_command = '/help'
         if message.text == start_command:
-            bot.reply_to(message, ' welcome to private chat')
+            bot.reply_to(message, start_private_msg)
         elif message.text == help_command:
-            bot.reply_to(message, help_msg)
+            bot.reply_to(message, help_private_msg)
